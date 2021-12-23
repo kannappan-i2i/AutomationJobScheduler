a80fb8776bee63dcc9f7083a5563c10b54436ce7
Kannappan Chidambaram <95862290+kannappan-i2i@users.noreply.github.com>
2021-12-23 18:28:49 +0530

    Update Timeslot.java

--- src/main/java/com/automationjobscheduler/domain/Timeslot.java
+++ src/main/java/com/automationjobscheduler/domain/Timeslot.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2021 Red Hat, Inc. and/or its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.automationjobscheduler.domain;
 
@@ -34,19 +20,30 @@ public class Timeslot {
     private DayOfWeek dayOfWeek;
     private LocalTime startTime;
     private LocalTime endTime;
+    private Integer timeRange;
+
+
+    public Integer getTimeRange() {
+        return timeRange;
+    }
+
+    public void setTimeRange(Integer timeRange) {
+        this.timeRange = timeRange;
+    }
 
     // No-arg constructor required for Hibernate
     public Timeslot() {
     }
 
-    public Timeslot(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+    public Timeslot(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime, Integer timeRange) {
         this.dayOfWeek = dayOfWeek;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.timeRange = timeRange;
     }
 
-    public Timeslot(long id, DayOfWeek dayOfWeek, LocalTime startTime) {
-        this(dayOfWeek, startTime, startTime.plusMinutes(50));
+    public Timeslot(long id, DayOfWeek dayOfWeek, LocalTime startTime, Integer timeRange) {
+        this(dayOfWeek, startTime, startTime.plusMinutes(50), timeRange);
         this.id = id;
     }
 
